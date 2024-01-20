2196acbe2d41d29614b8c4e26ba530a65f24169f
Anas Saoui <saouianas@hotmail.fr>
2024-01-20 09:44:01 +0000

    functionality added

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -4,11 +4,36 @@
 pragma solidity ^0.8.17;
 
 contract Admin {
+    uint a;
+    uint b;
+    uint c;
+    uint d;
+    address owner;
+
     constructor(){
         admin = msg.sender
     }
     modifier onlyAdmin{
         require(admin == msg.sender, 'only admin');
     }
-    
+        function set (uint _a, uint _b, uint _c, uint _d) public onlyOwner{
+        a = _a;
+        b = _b;
+        c = _c;
+        d = _d;
+    }
+     function get() public view onlyAdmin returns(uint){
+        if ((a > b) && (a > c) && (a > d)){
+        return(a);
+        }
+        else if ((b >c ) && (b > d) && (b > a)){
+            return(b);
+        }
+        else if ((c > d) && (c > a) && (c > b)){
+            return(c);
+        }
+        else {
+            return(d);
+        }
+    }
 }
